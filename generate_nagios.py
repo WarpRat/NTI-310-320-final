a5662f8bebb850671ea26c1c2fd34de9a05fdc2b
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2018-06-11 21:48:19 -0700

    fix gcloud scp

--- generate_nagios.py
+++ generate_nagios.py
@@ -2,10 +2,10 @@ import os
 from pprint import pprint
 #import requests
 
-def write_nagios_cfg(hosts, nagios_ip):
+def write_nagios_cfg(hosts, nagios_name, zone):
     if not os.path.isdir('tmp'):
         os.mkdir('tmp')
     for i in hosts:
         os.system('./scripts/generate_nagios_conf.sh %s %s' % (i['name'], i['ip']))
-        os.system('gcloud compute scp ./tmp/* %s:/etc/nagios/conf.d/' % nagios_ip)
+        os.system('gcloud compute scp ./tmp/* %s:/etc/nagios/conf.d/ --zone=%s' % (nagios_name, zone))
     
--- main.py
+++ main.py
@@ -447,7 +447,7 @@ if __name__ == '__main__':
   for i in all_hosts:
     to_mon.append({'name': i['name'], 'ip': i['ip']})
 
-  generate_nagios.write_nagios_cfg(to_mon, nagios_info['ip'])
+  generate_nagios.write_nagios_cfg(to_mon, nagios_info['name'], zone)
   
   command = "'sudo yum update -y && sudo yum install nti320pkg'"
 
